52b40f91c9877269511ee6f20e968c6035dd744f
cmg8431 <cmg8431@gmail.com>
2023-01-10 18:36:19 +0900

    feat(guide): service 개발 상황 안내 문구 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
 import React, { NextPage } from 'next';
 
+import * as S from './styled';
+
 const HomePage: NextPage = () => {
-  return <div>Welcome to Next.js with Yarn Berry!</div>;
+  return (
+    <S.PreparationGuideWrapper>
+      <p>
+        아직 서비스 준비 중이에요!
+        <br /> 곧 좋은 모습으로 찾아 뵐게요 :)
+      </p>
+    </S.PreparationGuideWrapper>
+  );
 };
 
 export default HomePage;
--- src/pages/styled.ts
+++ src/pages/styled.ts
@@ -1,1 +1,13 @@
-export {};
+import { styled } from '@/stitches.config';
+
+export const PreparationGuideWrapper = styled('div', {
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  textAlign: 'center',
+  fontWeight: '400',
+  fontSize: '1.2rem',
+  letterSpacing: '-0.02rem',
+});
