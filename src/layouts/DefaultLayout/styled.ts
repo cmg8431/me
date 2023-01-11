d6d643a220c0325ef9907ed4f5aff5d7a7f29749
cmg8431 <cmg8431@gmail.com>
2023-01-11 22:45:42 +0900

    feat(layout): default 레이아웃 추가

---
+++ src/layouts/DefaultLayout/styled.ts
@@ -0,0 +1,16 @@
+import { styled } from '@/stitches.config';
+
+export const DefaultLayoutContainer = styled('div', {
+  width: '100%',
+  height: '100%',
+});
+
+export const DefaultLayout = styled('div', {
+  display: 'flex',
+  height: '100%',
+  flex: 1,
+});
+
+export const DefaultLayoutWrapper = styled('main', {
+  width: '100%',
+});
