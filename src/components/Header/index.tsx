167eb6812ff99ba81dbddcfca221fe3fee0d28c1
cmg8431 <cmg8431@gmail.com>
2023-01-11 21:02:23 +0900

    feat(component): Header 컴포넌트 및 기본 세팅 추가

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './styled';
+
+export const Header = () => {
+  return (
+    <S.HeaderContainer>
+      <div>left</div>
+      <div>center</div>
+      <div>right</div>
+    </S.HeaderContainer>
+  );
+};
---
+++ src/components/Header/styled.ts
@@ -0,0 +1,10 @@
+import { styled } from '@/stitches.config';
+
+export const HeaderContainer = styled('header', {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  padding: '0 1rem 0 1rem',
+  height: '55px',
+  boxShadow: '0 1px 5px 0 rgb(107 119 172 / 20%)',
+});
