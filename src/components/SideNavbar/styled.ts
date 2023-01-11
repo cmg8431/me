9a88746c46ddbccbe72a302392dd681f014a449c
cmg8431 <cmg8431@gmail.com>
2023-01-11 22:38:16 +0900

    feat(components): `SideNavbar` 컴포넌트 및 기본 세팅 추가

---
+++ src/components/SideNavbar/styled.ts
@@ -0,0 +1,8 @@
+import { styled } from '@/stitches.config';
+
+export const SideNavBarContainer = styled('nav', {
+  width: '250px',
+  padding: '0.8rem 1.8rem 0',
+  borderRight: '1px solid rgba(48, 48, 56, 0.15)',
+  backgroundColor: '#f6f7fa',
+});
