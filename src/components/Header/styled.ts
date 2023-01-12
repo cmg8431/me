9674821e8ba5260bd9997fc9dc1a2150c35485fe
cmg8431 <cmg8431@gmail.com>
2023-01-12 21:45:30 +0900

    feat: default 디자인 요소 추가 및 디테일 수정

--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -4,7 +4,35 @@ export const HeaderContainer = styled('header', {
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
-  padding: '0 1rem 0 1rem',
-  height: '55px',
-  boxShadow: '0 1px 5px 0 rgb(107 119 172 / 20%)',
+  padding: '0 1.5rem 0 1.5rem',
+  height: '60px',
+  borderBottom: '1px solid #e5e8eb',
+});
+
+export const HeaderMenuContainer = styled('ul', {
+  display: 'flex',
+  gap: '2rem',
+});
+
+export const HeaderMenuItem = styled('li', {
+  fontSize: '0.96rem',
+  color: '#cecece',
+
+  variants: {
+    location: {
+      true: {
+        color: 'black',
+      },
+    },
+  },
+});
+
+export const CoffieButton = styled('button', {
+  transition: 'background .2s ease,color .1s ease',
+  border: '0 solid transparent',
+  borderRadius: '0.5rem',
+  fontSize: '0.95rem',
+  fontWeight: '500',
+  color: '#4f5052',
+  padding: '0.3rem 0.7rem',
 });
