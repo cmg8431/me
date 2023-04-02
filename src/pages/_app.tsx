fd7182398d005a6cc26d0503c843874aed2934df
cmg8431 <cmg8431@gmail.com>
2023-04-02 19:46:04 +0900

    feat(pages): `_app`에 전역적으로 fortawesome 셋업

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,15 @@ import { ThemeProvider } from 'next-themes';
 import { AppProps } from 'next/app';
 
 import { globalStyles, darkTheme } from '@/stitches.config';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
+config.autoAddCss = false;
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   globalStyles();
   return (
     <ThemeProvider
-      disableTransitionOnChange
       attribute="class"
       value={{ light: 'light', dark: darkTheme.className }}
       defaultTheme="system"
