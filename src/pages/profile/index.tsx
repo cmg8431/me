a5408b3d1d718e9b16a1468458516ef371cd8014
cmg8431 <cmg8431@gmail.com>
2023-04-12 21:39:10 +0900

    feat(pages): `Profile` 페이지 셋업

---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,11 @@
+import React, { NextPage } from 'next';
+
+import { DefaultLayout } from '@/layouts';
+
+import * as S from './styled';
+
+const ProfilePage: NextPage = () => {
+  return <DefaultLayout>test</DefaultLayout>;
+};
+
+export default ProfilePage;
