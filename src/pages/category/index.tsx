753f73c691dbdc0076fb4d3f3512a2f86d49200f
cmg8431 <cmg8431@gmail.com>
2023-06-11 17:24:33 +0900

    feat: layout 추가 및 서비스 준비 중 문구 추가

--- src/pages/category/index.tsx
+++ src/pages/category/index.tsx
@@ -4,7 +4,7 @@ import { AppLayout } from '@/components/layouts';
 
 import * as S from './styled';
 
-const ProfilePage: NextPage = () => {
+const CategoryPage: NextPage = () => {
   return (
     <AppLayout>
       아직 서비스 준비 중이에요! <br /> 곧 좋은 모습으로 찾아 뵐게요 :)
@@ -12,4 +12,4 @@ const ProfilePage: NextPage = () => {
   );
 };
 
-export default ProfilePage;
+export default CategoryPage;
