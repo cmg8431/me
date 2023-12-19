e9b7cac37873cafb88c5738904cd12dab8c5ed31
cmg8431 <cmg8431@gmail.com>
2023-12-19 15:55:00 +0900

    chore(deps): `vanilla-extract` 라이브러리 추가 및 셋업 반영

--- next.config.js
+++ next.config.js
@@ -1,13 +1,21 @@
 /** @type {import('next').NextConfig} */
 
+const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
+
+const withVanillaExtract = createVanillaExtractPlugin();
+
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 });
 
-module.exports = withMDX({
-  pageExtensions: ['tsx', 'md', 'mdx'],
-  webpack: (config, { isServer }) => {
-    if (!isServer) config.resolve.fallback.fs = false;
-    return config;
-  },
-});
+module.exports = withVanillaExtract(
+  withMDX({
+    pageExtensions: ['tsx', 'md', 'mdx'],
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback.fs = false;
+      }
+      return config;
+    },
+  })
+);
